e4354d5acc4dbed0780e24da3219f6e659bfcf94
munifsoleh0805 <70565611+munifsoleh0805@users.noreply.github.com>
2020-10-16 14:58:18 +0700

    finish project

---
+++ php_browser_persistent/practice/password.php
@@ -0,0 +1,12 @@
+<?php
+
+$password="paSsword";
+
+$encrypt_password=password_hash($password,PASSWORD_BCRYPT);
+echo $encrypt_password;
+
+if(password_verify('paSsword',$encrypt_password)){
+    echo "You have access to this page";
+}else{
+    echo "You dont have access to this page";
+}
